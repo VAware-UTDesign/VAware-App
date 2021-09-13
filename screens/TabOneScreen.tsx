84786c7d7e68bec485e7856b728b04f9b862edb1
xyzsid <sid.dommeti@gmail.com>
2021-09-13 14:02:47 -0500

    added button

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Alert, Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -9,6 +9,13 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
         <Text style={styles.title}>VAWARE</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Connect"
+            color="white"
+            onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
     </View>
   );
 }
@@ -16,10 +23,21 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container :{
     backgroundColor: '#0078DC',
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
   },
   title: {
     fontSize: 60,
     textAlign: 'center',
+  },
+  button: {
+    color: 'grey',
+  },
+  buttonContainer: {
+    width: '60%',
+    backgroundColor: 'grey',
+    borderRadius: 15,
+    color: 'white',
+    alignItems: 'center',
   }
 });
