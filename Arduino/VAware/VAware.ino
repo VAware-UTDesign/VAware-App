f3fb2a5da1652113bfcd965b0166b7273e042623
xyzsid <sid.dommeti@gmail.com>
2021-09-30 01:51:56 -0500

    made several refactorings

--- Arduino/VAware/VAware.ino
+++ Arduino/VAware/VAware.ino
@@ -7,6 +7,8 @@
 
 #define LED_BUILTIN 2
 #define pass (void)0
+#define BASE_VAL "0"
+#define TIME_DELAY 7000
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pLedBuiltinCharacteristic = NULL;
@@ -15,32 +17,57 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 
 // Generated UUID's for different Charateristics
-#define SERVICE_UUID        "e472cea9-3ae8-4d96-951e-7086fe17d416"
-#define LED_BUILTIN_UUID    "7abd909a-a9e5-4409-96a9-7aa4fa33426f"
-
+#define SERVICE_UUID          "e472cea9-3ae8-4d96-951e-7086fe17d416"
+#define LED_BUILTIN_UUID      "7abd909a-a9e5-4409-96a9-7aa4fa33426f"
 
 // A call back function to check if device is still connected or not
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
     deviceConnected = true;
   };
-  
-  void onDisconnect(BLEServer* pserver) {
+
+  void onDisconnect(BLEServer *pServer)
+  {
     deviceConnected = false;
   }
+//  void onWrite(BLECharacteristic *pCharacteristic) {
+//      std::string rxValue = pCharacteristic->getValue();
+//
+//      if (rxValue.length() > 0) {
+//        Serial.println("*********");
+//        Serial.print("Received Value: ");
+//        for (int i = 0; i < rxValue.length(); i++)
+//          Serial.print(rxValue[i]);
+//
+//        Serial.println();
+//        Serial.println("*********");
+//      }
+//    }
 };
 
 // Init PINS: assign the pins for the LEDs:
-int led_pin_1 = 21;
-int led_pin_2 = 19;
-int led_pin_3 = 18;
+int ledPin1 = 21;
+int ledPin2 = 19;
+int ledPin3 = 18;
 
-void setup() {
+void pnuematicsOperate(int ledPin, String message)
+{
+  digitalWrite(ledPin, HIGH);
+  Serial.println(message);
+  delay(TIME_DELAY);
+  digitalWrite(ledPin, LOW);
+  pLedBuiltinCharacteristic->setValue(BASE_VAL);
+}
+
+void setup()
+{
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(led_pin_1, OUTPUT);
-  pinMode(led_pin_2, OUTPUT);
-  pinMode(led_pin_3, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 
   //Create the BLE DEVICE!
   BLEDevice::init("VAware");
@@ -55,10 +82,10 @@ void setup() {
 
   //Create the BLE Charateristic for LED_BUILTIN
   pLedBuiltinCharacteristic = pService->createCharacteristic(
-                            LED_BUILTIN_UUID,
-                            BLECharacteristic::PROPERTY_READ    |
-                            BLECharacteristic::PROPERTY_WRITE
-                          );
+          LED_BUILTIN_UUID,
+          BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE
+  );
   //Might need a Descriptor. DONT KNOW WHAT THAT IS :(
 
   pLedBuiltinCharacteristic->setValue("Heyo says sid");
@@ -68,61 +95,56 @@ void setup() {
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  //pAdvertising->setMinPreferred(); 
+  //pAdvertising->setMinPreferred();
   BLEDevice::startAdvertising();
   Serial.println("Waiting a client connection to notify...");
 }
 
-void loop() {
-  //The loop function
-  if (deviceConnected) {
+void loop()
+{
+  if (deviceConnected)
+  {
     digitalWrite(LED_BUILTIN, HIGH);
-    //delay(500);
     std::string value = pLedBuiltinCharacteristic->getValue();
-    
-    if (value == "1") {
-      digitalWrite(led_pin_1, HIGH);
-      Serial.println("GREEN");
-      delay(10000);
-      digitalWrite(led_pin_1, LOW);
-      pLedBuiltinCharacteristic->setValue("0");
+
+    if (value == "1")
+    {
+      pnuematicsOperate(ledPin1, "INFLATEEEE");
     }
-    else if (value == "2") {
-      digitalWrite(led_pin_2, HIGH);
+    else if (value == "2")
+    {
+      digitalWrite(ledPin2, HIGH);
       Serial.println("RED");
       delay(7000);
-      digitalWrite(led_pin_2, LOW);
+      digitalWrite(ledPin2, LOW);
       pLedBuiltinCharacteristic->setValue("0");
+//      pnuematicsOperate(ledPin2, "INFLATEEEE");
     }
-    else if (value == "3") {
-      digitalWrite(led_pin_3, HIGH);
+    else if (value == "3")
+    {
+      digitalWrite(ledPin3, HIGH);
       Serial.println("BLUE");
       delay(7000);
-      digitalWrite(led_pin_3, LOW);
+      digitalWrite(ledPin3, LOW);
       pLedBuiltinCharacteristic->setValue("0");
+//      pnuematicsOperate(ledPin3, "INFLATEEEE");
     }
-    else {
+    else
+    {
       pass;
     }
-//    else if (value == "4") {
-//      digitalWrite(led_pin_4, HIGH);
-//      delay(7000);
-//      digitalWrite(led_pin_3, LOW);
-//      pLedBuiltinCharacteristic->setValue("0");
-//    }
-    
-    
   }
 
-  if (!deviceConnected && oldDeviceConnected) {
-    //delay(500);
+  if (!deviceConnected && oldDeviceConnected)
+  {
     pServer->startAdvertising();
     Serial.println("started advertising");
     digitalWrite(LED_BUILTIN, LOW);
     oldDeviceConnected = deviceConnected;
   }
 
-  if (deviceConnected && !oldDeviceConnected) {
+  if (deviceConnected && !oldDeviceConnected)
+  {
     oldDeviceConnected = deviceConnected;
   }
 }
